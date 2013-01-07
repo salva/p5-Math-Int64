9e61d72c202e2f3582d001de2ca0f2e3775b8c7e
Salvador Fandino <sfandino@yahoo.com>
2013-01-07 14:00:44 +0100

    64bit native math is broken on 5.6

--- lib/Math/Int64/native_if_available.pm
+++ lib/Math/Int64/native_if_available.pm
@@ -1,7 +1,7 @@
 package Math::Int64::native_if_available;
 
 sub import {
-    if (Math::Int64::_backend() eq 'IV') {
+    if (Math::Int64::_backend() eq 'IV' and $] >= 5.008) {
         Math::Int64::_set_may_use_native(1);
         $^H{Math::Int64::native_if_available} = 1;
     }
