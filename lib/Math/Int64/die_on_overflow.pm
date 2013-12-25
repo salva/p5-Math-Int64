01fdaf60b75c88c8d2c4f0ee38b4a3e58a5941c1
Dave Rolsky <autarch@urth.org>
2013-12-25 13:27:50 -0600

    Work in progress on dzilation

--- lib/Math/Int64/die_on_overflow.pm
+++ lib/Math/Int64/die_on_overflow.pm
@@ -13,11 +13,9 @@ sub unimport {
 
 1;
 
+# ABSTRACT: catch overflows when using Math::Int64
+
 __END__
-
-=head1 NAME
-
-Math::Int64::die_on_overflow - catch overflows when using Math::Int64
 
 =head1 SYNOPSIS
 
@@ -31,14 +29,5 @@ Math::Int64::die_on_overflow - catch overflows when using Math::Int64
 =head1 SEE ALSO
 
 L<Math::Int64>.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright E<copy> 2011 by Salvador FandiE<ntilde>o
-(sfandino@yahoo.com)
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.8 or,
-at your option, any later version of Perl 5 you may have available.
 
 =cut
