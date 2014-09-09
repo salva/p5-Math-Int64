d02de6ed9d03266ed57f8632595b1c4db5455077
Salvador Fandino <sfandino@yahoo.com>
2014-09-09 16:16:35 +0200

    prepare for release 0.32

--- lib/Math/Int64.pm
+++ lib/Math/Int64.pm
@@ -549,7 +549,7 @@ L<Math::BigInt::GMP>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright E<copy> 2007, 2009, 2011-2013 by Salvador FandiE<ntilde>o
+Copyright E<copy> 2007, 2009, 2011-2014 by Salvador FandiE<ntilde>o
 (sfandino@yahoo.com)
 
 This library is free software; you can redistribute it and/or modify
